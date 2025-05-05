483dcf0819549391dfe333cb81ba4c3c6fe24fcf
mtshep <callum.shepherd@manxtelecom.com>
2025-05-05 13:25:39 +0100

    delte

--- commands/info/test.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('test')
-    .setDescription('Test Command')
-    .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
-  async execute(interaction) {},
-};
