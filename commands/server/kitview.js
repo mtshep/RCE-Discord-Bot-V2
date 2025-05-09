c48407fcbd5a4548a7907be3441049be4a0270d2
mtshep <callum.shepherd@manxtelecom.com>
2025-05-09 21:32:38 +0100

    f

--- commands/server/kitview.js
+++ commands/server/kitview.js
@@ -16,7 +16,7 @@ module.exports = {
 
     try {
       // Send the command to get the list of kits
-      const kitListRaw = await interaction.client.rce.command.send(
+      const kitListRaw = await interaction.client.rce.commands.send(
         server.identifier,
         'kit list'
       );
@@ -27,7 +27,7 @@ module.exports = {
       const embeds = [];
 
       for (const kitName of kitListResponse) {
-        const kitInfoRaw = await interaction.client.rce.command.send(
+        const kitInfoRaw = await interaction.client.rce.commands.send(
           server.identifier,
           `kit info "${kitName}"`
         );
