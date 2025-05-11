39e46f8fac2aefca750b3159a2f1fe607d2abff9
mtshep <callum.shepherd@manxtelecom.com>
2025-05-11 08:08:49 +0100

    up

---
+++ node_modules/rce.js/dist/logger/interfaces.d.ts
@@ -0,0 +1,11 @@
+export interface ILogger {
+    error: (message: string) => void;
+    warn: (message: string) => void;
+    info: (message: string) => void;
+    debug: (message: string) => void;
+}
+export interface LogType {
+    prefix: string;
+    emoji: string;
+    color: string;
+}
